3bbe84df213e25fcc4d2dde1a61b3da14452ef6e
osdf <osendorf@gmail.com>
2013-02-24 23:57:14 +0100

    Cython bindings.

---
+++ py_quic/setup.py
@@ -0,0 +1,31 @@
+import numpy
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import sys
+
+
+if sys.platform == 'darwin':
+    ext_modules = [Extension(
+        name="py_quic",
+        sources=["QUIC.C", "py_quic.pyx"],
+        include_dirs = [numpy.get_include()],
+        extra_compile_args=['-faltivec', '-I/System/Library/Frameworks/vecLib.framework/Headers'],
+        extra_link_args=["-Wl,-framework", "-Wl,Accelerate"],
+        language="c++"
+        )]
+else:
+    ext_modules = [Extension(
+        name="py_quic",
+        sources=["QUIC.C", "py_quic.pyx"],
+        include_dirs = [numpy.get_include(), "/usr/local/include"],
+        extra_compile_args=['-msse2', '-O2', '-fPIC', '-w'],
+        extra_link_args=["-llapack"],
+        language="c++"
+        )]
+
+setup(
+    name = "_quic",
+    cmdclass = {"build_ext": build_ext},
+    ext_modules = ext_modules
+    )
